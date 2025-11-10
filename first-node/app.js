6581115d353d8e264c377a6adaf195d8ef002524
hirodinn <hirebikila1@gmail.com>
2025-11-10 21:46:59 +0300

    work with the filesystem module which the functions come in pairs sync and async

--- first-node/app.js
+++ first-node/app.js
@@ -1,3 +1,9 @@
-import os from "os";
-console.log(`free memory in MB: ${os.freemem() / (1024 * 1024)}`);
-console.log(os.totalmem());
+import fs from "fs";
+
+console.log(fs.readdirSync("./")); //this is the synchronus so that it pauses the function run so it comes before the console displays the finished
+
+fs.readdir("./", (err, files) => {
+  console.log(files); //this is asynchrounus so it doesn't pause just start excution and continue to the rest of the code so this comes after the finished statement
+});
+
+console.log("finished");
