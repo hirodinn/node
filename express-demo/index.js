79c4db0e350f5717acdba2113c4f548e06749482
hirodinn <hirebikila1@gmail.com>
2025-11-13 00:49:46 +0300

    make sure to use the environment variables

--- express-demo/index.js
+++ express-demo/index.js
@@ -8,4 +8,6 @@ app.get("/api/courses", (req, res) => {
   res.send(["maths", "science", "sport"]);
 });
 
-app.listen(3000, () => console.log("listening to port 3000..."));
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`listening to port ${port}...`));
