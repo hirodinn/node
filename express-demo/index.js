12d9f0199a9c38eb6af8a8e64744af674ca866de
hirodinn <hirebikila1@gmail.com>
2025-11-13 01:25:52 +0300

    make the server to response based on the id of the course that is searched

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,21 +1,29 @@
 import express from "express";
 const app = express();
 
+const courses = [
+  { id: 1, name: "course 1" },
+  { id: 2, name: "course 2" },
+  { id: 3, name: "course 3" },
+];
+
 app.get("/", (req, res) => {
   res.send("hello ladies and gentlemen");
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send(["maths", "science", "sport"]);
+  res.send(courses);
 });
 
 app.get("/api/courses/:id", (req, res) => {
-  res.send(req.params);
+  const course = courses.find((c) => c.id === Number(req.params.id));
+  if (!course) res.status(404).send("the course with this id is not found");
+  res.send(course);
 });
 
-app.get("/api/users", (req, res) => {
-  res.send(req.query);
-});
+// app.get("/api/users", (req, res) => {
+//   res.send(req.query);
+// });
 
 const port = process.env.PORT || 3000;
 
