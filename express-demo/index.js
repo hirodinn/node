cbe5ec3e6c0fa40ee02c21c4825836a71b6ff4b4
hirodinn <hirebikila1@gmail.com>
2025-11-13 00:39:21 +0300

    add another route for my server

--- express-demo/index.js
+++ express-demo/index.js
@@ -4,5 +4,8 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("hello ladies and gentlemen");
 });
+app.get("/api/courses", (req, res) => {
+  res.send(["maths", "science", "sport"]);
+});
 
 app.listen(3000, () => console.log("listening to port 3000..."));
