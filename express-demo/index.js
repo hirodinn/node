14c06660c7b480de5bf404ae5694d283f6835e46
hirodinn <hirebikila1@gmail.com>
2025-11-13 01:14:00 +0300

    use query parameters

--- express-demo/index.js
+++ express-demo/index.js
@@ -4,8 +4,17 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("hello ladies and gentlemen");
 });
+
 app.get("/api/courses", (req, res) => {
   res.send(["maths", "science", "sport"]);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  res.send(req.params);
+});
+
+app.get("/api/users", (req, res) => {
+  res.send(req.query);
 });
 
 const port = process.env.PORT || 3000;
