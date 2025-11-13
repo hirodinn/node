4811952ba2a5c350564303c36eb460553f0fe1e5
hirodinn <hirebikila1@gmail.com>
2025-11-13 15:36:08 +0300

    add the put request and then refactor the code to avoid duplications

--- express-demo/index.js
+++ express-demo/index.js
@@ -9,6 +9,10 @@ const courses = [
   { id: 3, name: "course 3" },
 ];
 
+const courseSchema = Joi.object({
+  name: Joi.string().min(3).required(),
+});
+
 app.get("/", (req, res) => {
   res.send("hello ladies and gentlemen");
 });
@@ -24,18 +28,27 @@ app.get("/api/courses/:id", (req, res) => {
 });
 
 app.post("/api/courses", (req, res) => {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-  });
-  const result = schema.validate(req.body);
-  if (result.error) {
-    res.status(400).send(result.error.details[0].message);
+  const { error } = courseSchema.validate(req.body || {});
+  if (error) {
+    res.status(400).send(error.details[0].message);
     return;
   }
   const course = { id: courses.length + 1, name: req.body.name };
   courses.push(course);
   res.send(course);
   console.log(courses);
+});
+
+app.put("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === Number(req.params.id));
+  if (!course) res.status(404).send("the course with this id is not found");
+  const { error } = courseSchema.validate(req.body || {});
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+  course.name = req.body.name;
+  res.send(course);
 });
 
 // app.get("/api/users", (req, res) => {
