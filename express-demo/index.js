64c7db41fc641f99cbecf95324ba1691207e41ff
hirodinn <hirebikila1@gmail.com>
2025-11-13 13:22:53 +0300

    create a post request

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 const app = express();
+app.use(express.json());
 
 const courses = [
   { id: 1, name: "course 1" },
@@ -18,6 +19,12 @@ app.get("/api/courses", (req, res) => {
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === Number(req.params.id));
   if (!course) res.status(404).send("the course with this id is not found");
+  res.send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+  const course = { id: courses.length + 1, name: req.body.name };
+  courses.push(course);
   res.send(course);
 });
 
