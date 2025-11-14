efb7d1066bae1018504b7d72957fe219f23a669e
hirodinn <hirebikila1@gmail.com>
2025-11-14 19:49:47 +0300

    add a get request api to return all users from the databse

--- postgres-demo/index.js
+++ postgres-demo/index.js
@@ -4,6 +4,19 @@ import { pool } from "./db.js";
 const app = express();
 app.use(express.json()); // Parses incoming JSON bodies
 
+//get
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await pool.query("SELECT * FROM users");
+    res.json(users.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+//port
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
