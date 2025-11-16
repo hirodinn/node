ce4bab8d5d0f946309166a13f615c42ff1414972
hirodinn <hirebikila1@gmail.com>
2025-11-16 22:38:48 +0300

    add promise all

--- promise/index.js
+++ promise/index.js
@@ -1,5 +1,19 @@
-const p = new Promise((resolve, reject) => {
+// const p = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(123);
+//   }, 1000);
+// }).then((result) => console.log(result));
+
+const p1 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve(123);
-  }, 1000);
-}).then((result) => console.log(result));
+    console.log("loaded a user from database");
+    resolve(1);
+  }, 2000);
+});
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("loaded a user from github");
+    resolve(2);
+  }, 3000);
+});
+Promise.all([p1, p2]).then((result) => console.log(result));
