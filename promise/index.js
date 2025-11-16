049003bd2fc6f7cfdd6df24e3c951ad0ccfff0e0
hirodinn <hirebikila1@gmail.com>
2025-11-16 22:44:33 +0300

    use async await

--- promise/index.js
+++ promise/index.js
@@ -4,16 +4,28 @@
 //   }, 1000);
 // }).then((result) => console.log(result));
 
-const p1 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    console.log("loaded a user from database");
-    resolve(1);
-  }, 2000);
-});
-const p2 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    console.log("loaded a user from github");
-    resolve(2);
-  }, 3000);
-});
-Promise.race([p1, p2]).then((result) => console.log(result));
+// const p1 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     console.log("loaded a user from database");
+//     resolve(1);
+//   }, 2000);
+// });
+// const p2 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     console.log("loaded a user from github");
+//     resolve(2);
+//   }, 3000);
+// });
+// Promise.race([p1, p2]).then((result) => console.log(result));
+
+async function chronological() {
+  console.log("this is before the promise is started");
+  await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("the first promise is resolved");
+      resolve();
+    }, 1000);
+  });
+  console.log("after the promise is returned");
+}
+chronological();
