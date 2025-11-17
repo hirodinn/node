266054294cdefc18f2c98a243d04d17cc8336f23
hirodinn <hirebikila1@gmail.com>
2025-11-17 13:49:05 +0300

    create schema for my mongo db

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -4,3 +4,10 @@ mongoose
   .connect("mongodb://localhost/playground")
   .then(() => console.log("connected to mongo DB"))
   .catch((err) => console.log(err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now() },
+});
