bc059c4a07d8302f87a6345e6506f0247182b939
hirodinn <hirebikila1@gmail.com>
2025-11-20 16:41:06 +0300

    add the put route

--- express-demo/routes/customers.js
+++ express-demo/routes/customers.js
@@ -47,4 +47,19 @@ route.post("/", async (req, res) => {
     res.status(400).send(err.message);
   }
 });
+
+// put
+
+route.put("/:id", async (req, res) => {
+  const { error } = schema.validate(req.body || {});
+  if (error) return res.status(400).send(error.details[0].message);
+  try {
+    const result = await Customers.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.send(result);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
 export default route;
