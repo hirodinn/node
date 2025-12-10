7615dabac113ed37681441585f5bd8164d352c1e
hirodinn <hirebikila1@gmail.com>
2025-12-10 17:06:56 +0300

    create the error middleware

---
+++ vidly/middleware/error.js
@@ -0,0 +1,11 @@
+import logger from "../logger.js";
+
+export default function errorHandler(err, req, res, next) {
+  logger.error({
+    message: err.message,
+    stack: err.stack,
+    route: req.originalUrl,
+  });
+
+  res.status(500).send("Something went wrong");
+}
