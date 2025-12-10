a43618bb54995dbcc64f6078644ab03881010a80
hirodinn <hirebikila1@gmail.com>
2025-12-11 00:02:59 +0300

    modify the error message that is logged to the console

--- vidly/logger.js
+++ vidly/logger.js
@@ -7,7 +7,18 @@ const logger = winston.createLogger({
     winston.format.json()
   ),
   transports: [
-    new winston.transports.Console(),
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp({ format: "MMM-DD HH:mm:ss" }),
+        winston.format.printf((info) => {
+          if (info.stack) {
+            return `[${info.timestamp}] ${info.level} -> ${info.stack}`;
+          }
+          return `[${info.timestamp}] ${info.level} -> ${info.message}`;
+        })
+      ),
+    }),
     new winston.transports.File({ filename: "logs/error.log", level: "error" }),
     new winston.transports.File({ filename: "logs/info.log", level: "info" }),
     new winston.transports.File({ filename: "logs/combined.log" }),
